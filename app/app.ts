3e80b44871c772f0aa8831e0fbe69be87aa6c9a2
Marc Laval <mlaval@amadeus.com>
2017-03-20 14:43:53 +0100

    feat: make sample more interesting

--- app/app.ts
+++ app/app.ts
@@ -4,10 +4,17 @@ import {Component} from '@angular/core'
   selector: 'my-app',
   template: `<h1>Hello World</h1>
 Counter: {{counter}}<br>
-<button (click)="counter = counter - 1">Less</button>
-<button (click)="counter = counter + 1">More</button>
-`,
+<button (click)="counter = counter - 1" [disabled]="counter <= 0">
+    Less
+</button>
+<button (click)="increment()">
+    More
+</button>`,
 })
 export class App {
   counter: number = 0;
+
+  increment() {
+    this.counter++;
+  }
 }
